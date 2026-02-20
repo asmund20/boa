d1f1e1b0ec2e8ffbbcc6f3b466b3076ab2657889
Åsmund Olai Sand-Larsen <asmund.sl@proton.me>
2026-02-20 15:41:05 +0100

    Ignore build files, format interpreter file and fix build version issue

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -1,34 +1,35 @@
--- Skeleton file for Boa Interpreter. Edit only definitions with 'undefined'
-
 module Interpreter where
 
-import Syntax
-import Control.Monad
+-- Skeleton file for Boa Interpreter. Edit only definitions with 'undefined'
 
-type Output      = [String]
+import Control.Monad
+import Syntax
+
+type Output = [String]
 
 type ErrorMessage = String
-data RuntimeError =
-    UnboundVariable VariableName
-  | BadFunction     FunctionName
-  | BadArgument     ErrorMessage
-  deriving (Eq, Show)
+data RuntimeError
+    = UnboundVariable VariableName
+    | BadFunction FunctionName
+    | BadArgument ErrorMessage
+    deriving (Eq, Show)
 
 type Environment = [(VariableName, Value)]
-type Runtime a   = Environment -> (Either RuntimeError a, Output)
+type Runtime a = Environment -> (Either RuntimeError a, Output)
 
 newtype Boa a = Boa {run :: Runtime a}
 
 -- Monad instance.
 instance Monad Boa where
-  return = undefined
-  (>>=)  = undefined
+    return = undefined
+    (>>=) = undefined
 
 -- Freebies.
 instance Functor Boa where
-  fmap = liftM
+    fmap = liftM
 instance Applicative Boa where
-  pure = return; (<*>) = ap
+    pure = return
+    (<*>) = ap
 
 -- Operations of the Boa monad
 abort :: RuntimeError -> Boa a
@@ -56,21 +57,24 @@ apply = undefined
 -- Main functions of interpreter
 eval :: Expression -> Boa Value
 eval (Operation op e1 e2) =
-  do v1 <- eval e1
-     v2 <- eval e2
-     case operate op v1 v2 of
-       (Right value) -> return value
-       (Left  err  ) -> abort $ BadArgument err
+    do
+        v1 <- eval e1
+        v2 <- eval e2
+        case operate op v1 v2 of
+            (Right value) -> return value
+            (Left err) -> abort $ BadArgument err
 eval _ = undefined
 
 exec :: Program -> Boa ()
-exec [                       ] = return ()
+exec [] = return ()
 exec (Define x body : program) =
-  do v <- eval body
-     bind x v (exec program)
+    do
+        v <- eval body
+        bind x v (exec program)
 exec (Execute thing : program) =
-  do eval thing
-     exec program
+    do
+        eval thing
+        exec program
 
 execute :: Program -> (Output, Maybe RuntimeError)
 execute = undefined
