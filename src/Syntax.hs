eeca7c16f250ad683edbf03f146159d3f63c89a4
Åsmund Olai Sand-Larsen <asmund.sl@proton.me>
2026-02-25 19:00:54 +0100

    Format file

--- src/Syntax.hs
+++ src/Syntax.hs
@@ -2,50 +2,49 @@
 
 module Syntax where
 
-data Value =
-    None
-  | Boolean Bool
-  | Number  Integer
-  | Text    String
-  | List    [Value]
-  deriving (Eq, Show, Read)
+data Value
+    = None
+    | Boolean Bool
+    | Number Integer
+    | Text String
+    | List [Value]
+    deriving (Eq, Show, Read)
 
-data Expression =
-    Constant  Value
-  | Variable  VariableName
-  | Operation OperationSymbol Expression Expression
-  | Not Expression
-  | Call FunctionName FunctionInput
-  | ListExpression    [Expression]
-  | ListComprehension Expression [Clause]
-  deriving (Eq, Show, Read)
+data Expression
+    = Constant Value
+    | Variable VariableName
+    | Operation OperationSymbol Expression Expression
+    | Not Expression
+    | Call FunctionName FunctionInput
+    | ListExpression [Expression]
+    | ListComprehension Expression [Clause]
+    deriving (Eq, Show, Read)
 
-type VariableName      = String
-type FunctionName      = String
-type FunctionInput     = [Expression]
+type VariableName = String
+type FunctionName = String
+type FunctionInput = [Expression]
 type FunctionArguments = [Value]
 
-data OperationSymbol =
-    Plus
-  | Minus
-  | Times
-  | Div
-  | Mod
-  | Eq
-  | Less
-  | Greater
-  | In
-  deriving (Eq, Show, Read)
+data OperationSymbol
+    = Plus
+    | Minus
+    | Times
+    | Div
+    | Mod
+    | Eq
+    | Less
+    | Greater
+    | In
+    deriving (Eq, Show, Read)
 
-data Clause =
-    For VariableName Expression
-  | If               Expression
-  deriving (Eq, Show, Read)
+data Clause
+    = For VariableName Expression
+    | If Expression
+    deriving (Eq, Show, Read)
 
 type Program = [Statement]
 
-data Statement =
-    Define  VariableName Expression
-  | Execute              Expression
-  deriving (Eq, Show, Read)
-
+data Statement
+    = Define VariableName Expression
+    | Execute Expression
+    deriving (Eq, Show, Read)
