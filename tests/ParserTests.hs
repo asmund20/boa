4b309139d1efd75ae29c3b024766d67bd0f6983a
Åsmund Olai Sand-Larsen <asmund.sl@proton.me>
2026-03-14 16:50:56 +0100

    Finish changing type signatures

--- tests/ParserTests.hs
+++ tests/ParserTests.hs
@@ -46,6 +46,19 @@ sizedProgram decls 0 = oneof [assignment, expression]
     expression = do
         e <- sized $ sizedExpression decls
         return (e, emptyDecls)
+sizedProgram decls n = do
+    (s1, decls') <- sizedProgram decls 0
+    (ss, _) <- sizedProgram decls' (n - 1)
+    spaces1 <- genSpaces
+    spaces2 <- genSpaces
+    return (s1 ++ spaces1 ++ ';' : spaces2 ++ ss, emptyDecls)
+
+identGen :: Gen String
+identGen = do
+    c <- identStartGen
+    cs <- listOf identBodypartGen
+    return (c : cs)
+  where
     letterGen :: Gen Char
     letterGen = elements $ ['a' .. 'z'] ++ ['A' .. 'Z']
     identStartGen :: Gen Char
@@ -61,136 +74,214 @@ sizedProgram decls 0 = oneof [assignment, expression]
             , (1, return '_')
             , (1, elements ['0' .. '9'])
             ]
-sizedProgram decls n = do
-    (s1, decls') <- sizedProgram decls 0
-    (ss, _) <- sizedProgram decls' (n - 1)
-    spaces1 <- genSpaces
-    spaces2 <- genSpaces
-    return (s1 ++ spaces1 ++ ';' : spaces2 ++ ss, emptyDecls)
-
-identGen :: Gen String
-identGen = do
-    c <- identStartGen
-    cs <- listOf identBodypartGen
-    return (c : cs)
 
 sizedExpression :: Decls -> Int -> Gen String
-sizedExpression decls 0 = exprLiteral decls
-sizedExpression decls n = typedSizedExpression TAny decls n
+sizedExpression decls 0 = do
+    (e, _) <- exprLiteral decls
+    return e
+sizedExpression decls n = do
+    (e, _) <- typedSizedExpression TAny decls n
+    return e
 
 -- TODO: All typedSizedExpression should have the possibility of choosing an identifier with the correct type
 -- data BoaType = TNone | TBoolean | TNumber | TText | TList | TAny
-typedSizedExpression :: BoaType -> Decls -> Int -> Gen String
-typedSizedExpression TNone _ 0 = return "None"
-typedSizedExpression TBoolean _ 0 = oneof [return "True", return "False"]
-typedSizedExpression TNumber _ 0 = show <$> arbitrarySizedNatural
-typedSizedExpression TText _ 0 = sized sizedStringConst
-typedSizedExpression TList decls 0 = return "[]"
+typedSizedExpression :: BoaType -> Decls -> Int -> Gen (String, BoaType)
+typedSizedExpression TNone _ 0 = return ("None", TNone)
+typedSizedExpression TBoolean _ 0 = oneof [return ("True", TBoolean), return ("False", TBoolean)]
+typedSizedExpression TNumber _ 0 = do
+    n <- show <$> arbitrarySizedNatural
+    return $ (n, TNumber)
+typedSizedExpression TText _ 0 = do
+    s <- sized sizedStringConst
+    return $ (s, TText)
+typedSizedExpression TList decls 0 = return ("[]", TAny)
 typedSizedExpression TAny decls 0 = exprLiteral decls
 typedSizedExpression TAny decls n = do
     t <- genType
     typedSizedExpression t decls n
-typedSizedExpression TNone decls n = frequency [(1, return "None"), (2, printCall)]
+typedSizedExpression TNone decls n = frequency [(1, return ("None", TNone)), (2, printCall)]
   where
     printCall = do
         partitioning <- choose (1, 10)
         let expLength = n `div` partitioning
         let listLength = n - expLength
-        csv <- genCsvExprs TAny listLength decls expLength
+        (csv, _) <- genCsvExprs TAny listLength decls expLength
         s1 <- genSpaces
         s2 <- genSpaces
         s3 <- genSpaces
-        return $ "print" ++ s1 ++ "(" ++ s2 ++ csv ++ s3 ++ ")"
+        return ("print" ++ s1 ++ "(" ++ s2 ++ csv ++ s3 ++ ")", TNone)
 typedSizedExpression TList decls n = oneof [listExp, listComprehension, rangeCall]
   where
     listExp = do
         partitioning <- choose (1, 10)
         let expLength = n `div` partitioning
         let listLength = n - expLength
-        csv <- genCsvExprs TAny listLength decls expLength
+        (csv, t) <- genCsvExprs TAny listLength decls expLength
         s1 <- genSpaces
         s2 <- genSpaces
-        return $ '[' : s1 ++ csv ++ s2 ++ "]"
-    listComprehension = do
-        -- I want to do this by first generating the clauses.
-        -- The for clauses all generate a valid identifier for a
-        -- TNumber, these should be returned and used as the
-        -- environment, probably with a selection of the Number
-        -- identifiers that already exist for creating the expression
-        partitioning <- choose (1, 10)
-        let expLength = n `div` partitioning
-        let clauseLength = n - expLength
-        (e, c) <- genForClause -- unsure what type to use here
-        cs <- genClauses clauseLength expLength
-        s1 <- genSpaces
-        s2 <- genSpaces
-        s3 <- genSpaces
-        s4 <- genSpaces
-        return $ '[' : s1 ++ e ++ s2 ++ c ++ s3 ++ cs ++ s4 ++ "]"
+        return ('[' : s1 ++ csv ++ s2 ++ "]", t)
+    listComprehension = undefined {-do
+                                  -- I want to do this by first generating the clauses.
+                                  -- The for clauses all generate a valid identifier for a
+                                  -- TNumber, these should be returned and used as the
+                                  -- environment, probably with a selection of the Number
+                                  -- identifiers that already exist for creating the expression
+                                  partitioning <- choose (1, 10)
+                                  let expLength = n `div` partitioning
+                                  let clauseLength = n - expLength
+                                  (e, c) <- genForClause
+                                  cs <- genClauses clauseLength expLength
+                                  s1 <- genSpaces
+                                  s2 <- genSpaces
+                                  s3 <- genSpaces
+                                  s4 <- genSpaces
+                                  return $ '[' : s1 ++ e ++ s2 ++ c ++ s3 ++ cs ++ s4 ++ "]"
+                                  -}
     rangeCall = do
         params <- choose (1, 3)
-        csv <- genCsvExprs TNumber params decls (n `div` params)
+        (csv, _) <- genCsvExprs TNumber params decls (n `div` params)
         s1 <- genSpaces
         s2 <- genSpaces
-        return $ "range" ++ '(' : s1 ++ csv ++ s2 ++ ")"
-    genForClause :: Gen (String, Decls)
-    genForClause = do
+        return ("range" ++ '(' : s1 ++ csv ++ s2 ++ ")", TNumber)
+    genForClause :: Int -> Decls -> Gen (String, Decls)
+    genForClause expLength d = do
         i <- identGen
-        l <- typedSizedExpression TList decls (n `div` 2)
+        (l, t) <- typedSizedExpression TList d expLength
         s1 <- genSpaces
         s2 <- genSpaces
         s3 <- genSpaces
-        return ("for " ++ s1 ++ i ++ " in " ++ s2 ++ l, Map.insert i TAny emptyDecls)
-    genIfClause = undefined
-    genClauses clauseLength expLength = do
-        c <- oneof [genForClause genIfClause]
-        cs <- genClauses (clauseLength - 1) expLength
+        return ("for " ++ s1 ++ i ++ " in " ++ s2 ++ l, Map.insert i t d)
+    genIfClause :: Int -> Decls -> Gen (String, Decls)
+    genIfClause expLength d = do
+        (e, _) <- oneof [typedSizedExpression TAny d expLength, typedSizedExpression TBoolean d expLength]
         s1 <- genSpaces
-        return $ c ++ ' ' : s1 ++ cs
-typedSizedExpression t decls n = oneof [binaryExp, notExp, parenExp, sizedExpression decls 0]
+        return $ ("if " ++ s1 ++ e, decls)
+    genClauses clauseLength expLength = undefined {-do
+                                                  (c, _) <- oneof [genForClause genIfClause]
+                                                  cs <- genClauses (clauseLength - 1) expLength
+                                                  s1 <- genSpaces
+                                                  return $ c ++ ' ' : s1 ++ cs
+                                                  -}
+
+-- TODO: notExp is only for boolean and number if inside parens and any type
+typedSizedExpression t decls n = oneof [binaryExp, notExp, parenExp, typedSizedExpression t decls 0]
   where
     binaryExp = do
-        -- TODO: This must probably be a bit more sophisticated, Bools can be multiplicated with anything, resulting type is the non-bool or bool
-        -- anything can be compared size-wise except None, False == 0 and True == 1, anything can be compared for equality
-        e1 <- typedSizedExpression t decls (n `div` 2)
-        e2 <- typedSizedExpression t decls (n `div` 2)
+        (o, tl, tr) <- genOperator t
+        (el, _) <- typedSizedExpression tl decls (n `div` 2)
+        (er, _) <- typedSizedExpression tr decls (n `div` 2)
         s1 <- genSpaces
         s2 <- genSpaces
-        op <- genOperator t
-        return $ e1 ++ s1 ++ op ++ s2 ++ e2
+        return (el ++ s1 ++ o ++ s2 ++ er, t)
     notExp = do
-        e <- typedSizedExpression t decls (n - 1)
+        (e, _) <- typedSizedExpression t decls (n - 1)
         s <- genSpaces
-        return $ "not" ++ s ++ e
+        return ("not" ++ s ++ e, TBoolean)
     parenExp = do
-        e <- typedSizedExpression t decls (n - 1)
+        (e, _) <- typedSizedExpression t decls (n - 1)
         s1 <- genSpaces
         s2 <- genSpaces
-        return $ '(' : s1 ++ e ++ s2 ++ ")"
+        return ('(' : s1 ++ e ++ s2 ++ ")", t)
 
-genCsvExprs :: BoaType -> Int -> Decls -> Int -> Gen String
+genCsvExprs :: BoaType -> Int -> Decls -> Int -> Gen (String, BoaType)
 genCsvExprs t 0 decls exprSize = typedSizedExpression t decls exprSize
+genCsvExprs TAny n decls exprSize = do
+    t <- genType
+    genCsvExprs t n decls exprSize
 genCsvExprs t n decls exprSize = do
-    e <- typedSizedExpression t decls exprSize
-    es <- genCsvExprs t (n - 1) decls exprSize
+    (e, _) <- typedSizedExpression t decls exprSize
+    (es, _) <- genCsvExprs t (n - 1) decls exprSize
     s1 <- genSpaces
     s2 <- genSpaces
-    return $ e ++ s1 ++ ',' : s2 ++ es
+    return (e ++ s1 ++ ',' : s2 ++ es, t)
 
-genOperator :: BoaType -> Gen String
-genOperator = undefined
+-- | genOperator:  ResultType -> Gen (String, LeftArgtype, RightArgType)
+genOperator :: BoaType -> Gen (String, BoaType, BoaType)
+genOperator TBoolean = do
+    o <-
+        oneof
+            [ return "=="
+            , return "!="
+            , return "<"
+            , return "<="
+            , return ">"
+            , return ">="
+            , return "in"
+            , return "not in"
+            ]
+    case o of
+        "in" -> oneof [return (o, TAny, TList), return (o, TText, TText)]
+        "not in" -> oneof [return (o, TAny, TList), return (o, TText, TText)]
+        "<" -> do
+            t <- genType
+            return (o, t, t)
+        "<=" -> do
+            t <- genType
+            return (o, t, t)
+        ">" -> do
+            t <- genType
+            return (o, t, t)
+        ">=" -> do
+            t <- genType
+            return (o, t, t)
+        "==" -> return (o, TAny, TAny)
+        "!=" -> return (o, TAny, TAny)
+genOperator TNumber = do
+    o <- oneof [return "+", return "-", return "*", return "//", return "%"]
+    t1 <- frequency [(1, return TBoolean), (2, return TNumber)]
+    t2 <- frequency [(5, return TNumber), (1, return TBoolean)]
+    return (o, t1, t2)
+genOperator TText = do
+    o <- oneof [return "+", return "*"]
+    case o of
+        "+" -> return (o, TText, TText)
+        "*" -> do
+            l <- oneof [return TNumber, return TBoolean, return TText]
+            case l of
+                TText -> do
+                    r <- oneof [return TNumber, return TBoolean]
+                    return (o, TText, r)
+                _ -> return (o, l, TText)
+genOperator TList = do
+    o <- oneof [return "+", return "*"]
+    case o of
+        "+" -> return (o, TList, TList)
+        "*" -> do
+            l <- oneof [return TNumber, return TBoolean, return TList]
+            case l of
+                TList -> do
+                    r <- oneof [return TNumber, return TBoolean]
+                    return (o, TList, r)
+                _ -> return (o, l, TList)
+genOperator TAny = do
+    t <- oneof [return TBoolean, return TNumber, return TText, return TList]
+    genOperator t
+
+eqOperator :: Gen String
+eqOperator = oneof [return "==", return "!="]
+
+relOperator :: Gen String
+relOperator = oneof [return "<", return "<=", return ">", return ">="]
+
+inOperator :: Gen String
+inOperator = oneof [return "in", return "not in"]
 
 typeGen :: Gen BoaType
 typeGen = oneof [return TNone, return TBoolean, return TNumber, return TText, return TList]
 
-exprLiteral :: Decls -> Gen String
-exprLiteral decls = oneof [numConst, sized sizedStringConst, none, true, false, ident]
+exprLiteral :: Decls -> Gen (String, BoaType)
+exprLiteral decls = oneof [numConst, stringConst, none, true, false, ident]
   where
-    numConst = show <$> arbitrarySizedNatural
-    none = return "None"
-    true = return "True"
-    false = return "True"
-    ident = elements $ map first $ Map.toList decls
+    numConst = do
+        n <- show <$> arbitrarySizedNatural
+        return (n, TNumber)
+    stringConst = do
+        s <- sized sizedStringConst
+        return (s, TText)
+    none = return ("None", TNone)
+    true = return ("True", TBoolean)
+    false = return ("False", TBoolean)
+    ident = elements $ Map.toList decls
 
 sizedStringConst :: Int -> Gen String
 sizedStringConst size = do
